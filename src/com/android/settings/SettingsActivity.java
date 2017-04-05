43156071fa2f31b6af3a6b915e7cbedde06eb8b0
Dan Pasanen <dan.pasanen@gmail.com>
2017-04-05 08:46:36 -0500

    Merge tag 'android-7.1.2_r2' into cm-14.1
    Android 7.1.2 Release 2 (N2G47E)
    
    Change-Id: I45b519f43290a79b86f3976cddecf1a0199ced25
    Signed-off-by: Pafcholini <nadyaivanova14@gmail.com>

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -1236,7 +1236,8 @@ public class SettingsActivity extends SettingsDrawerActivity
             }
         }
         setTileEnabled(new ComponentName(packageName,
-                BackupSettingsActivity.class.getName()), hasBackupActivity, isAdmin, pm);
+                BackupSettingsActivity.class.getName()), hasBackupActivity,
+                isAdmin || Utils.isCarrierDemoUser(this), pm);
 
     }
 
